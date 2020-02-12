754e0a84cda805ba995a98f5a957727bb0c8f0f8
Oracle Public Cloud User <opc@kedastream.sub41303392c.cluster1.oraclevcn.com>
2020-02-12 11:01:38 +0000

    yum install jdk for jenkins

--- instance.tf
+++ instance.tf
@@ -171,6 +171,7 @@ provisioner "file" {
     inline = ["sudo yum -y update",
               "sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo",
               "sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key",
+	      "sudo yum -y install java-1.8.0-openjdk",
               "sudo yum -y install jenkins",
               "sudo service jenkins start",
 	      "sudo systemctl stop firewalld",
